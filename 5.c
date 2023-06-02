75c096b2fd07b7ce403a2e64552c310514424138
MichaelNoskov <michael.noskov@yandex.ru>
2023-06-02 15:17:38 +0300

    qqqq

--- 5.c
+++ 5.c
@@ -10,23 +10,24 @@ struct PC {
 struct PC* fastestPC(struct PC data[], int n) {
     int sp = 0, r = 0;
     struct PC *b;
+    struct PC u;
     for (int i=0; i<n; i++)   {
         struct PC computer = data[i];
-        printf("%d %d\n", computer.speed, sp);
         if (computer.speed > sp) {
             sp = computer.speed;
             r = computer.RAM;
-            b = &computer;
+            u  = computer;
+            b = &u;
         } else if (computer.speed == sp && computer.RAM > r) {
             r = computer.RAM;
-            b = &computer;
+            b = &u;
         }
     }
     return b;
 }
 
 int main() {
-    struct PC data[3] = {{"1", 3, 4}, {"2", 3, 1}, {"3", 0, 1}};
+    struct PC data[3] = {{"1", 3, 4}, {"2", 3, 1}, {"3", 5, 1}};
     struct PC a = *fastestPC(data, 3);
     printf("%d %d\n", a.speed, a.RAM);
     return 0;
